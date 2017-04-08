ba83b428d3c1d241e04cff5830d811be01c5f12f
vijendra Kulhade <vijendra07kulhade@gmail.com>
2017-04-07 23:11:39 -0700

    Updated with passport google

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
 var express = require('express');
+var session = require('express-session');
 
 var app = express();
 
 var port = process.env.PORT || 5000;
 
 var nav = [{
-    link: '/books',
-    text: 'Books'
+  link: '/books',
+  text: 'Books'
         }, {
-    link: '/authers',
-    text: 'Authors'
+  link: '/authers',
+  text: 'Authors'
         }, {
-    link: '/contact',
-    text: 'Contact Us'
+  link: '/contact',
+  text: 'Contact Us'
         }];
-var bookRouters = require('./src/routes/booksroutes')(nav);
+var index = require('./src/routes/index')(nav);
+var bookRoutes = require('./src/routes/booksroutes')(nav);
+var authRoutes = require('./src/routes/authRoutes');
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-app.use('/books', bookRouters);
 
-app.get('/', function (req, res) {
-    res.render('index', {
-        title: 'Library',
-        nav: nav
-    });
-});
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+require('./src/config/passport')(app);
+
+app.use('/', index);
+app.use('/auth', authRoutes);
+app.use('/books', bookRoutes);
 
 
 app.listen(port, function (err) {
-    console.log('running server on port ' + port);
+  console.log('running server on port ' + port);
 });
--- src/routes/booksroutes.js
+++ src/routes/booksroutes.js
@@ -5,47 +5,48 @@ var bookRouters = express.Router();
 //secret: Wr6F45QvPh3ZdC9Xi3L5xJCAKrDbLsDMTVbtT62OoEw
 
 var books = [{
-        id: 0,
-        name: 'Book_0',
-        description: 'This is book 0 decription'
+    id: 0,
+    name: 'Book_0',
+    description: 'This is book 0 decription'
             }, {
-        id: 1,
-        name: 'Book_1',
-        description: 'This is book 1 decription'
+    id: 1,
+    name: 'Book_1',
+    description: 'This is book 1 decription'
             }, {
-        id: 2,
-        name: 'Book_2',
-        description: 'This is book 2 decription'
+    id: 2,
+    name: 'Book_2',
+    description: 'This is book 2 decription'
             },
-    {
-        id: 3,
-        name: 'Book_3',
-        description: 'This is book 3 decription'
+  {
+    id: 3,
+    name: 'Book_3',
+    description: 'This is book 3 decription'
             }];
 
 var router = function (nav) {
-    bookRouters.route('/')
-        .get(function (req, res) {
-            res.render('books', {
-                title: 'All Books',
-                nav: nav,
-                books: books
-            });
-        });
 
-    bookRouters.route('/:id')
-        .get(function (req, res) {
-            var id = req.params.id;
-            res.render('bookInfo', {
-                title: 'All Books',
-                nav: nav,
-                book: books[id]
-            });
-        });
+  bookRouters.route('/')
+    .get(function (req, res) {
+      res.render('books', {
+        title: 'All Books',
+        nav: nav,
+        books: books
+      });
+    });
 
-    return bookRouters;
+  bookRouters.route('/:id')
+    .get(function (req, res) {
+      var id = req.params.id;
+      res.render('bookInfo', {
+        title: 'All Books',
+        nav: nav,
+        book: books[id]
+      });
+    });
 
-}
+  return bookRouters;
+
+};
 
 
 module.exports = router;
