d5b30588778550edfb3c682d1ab1ef129242ef6d
Vijendra Kulhade <Vijendra07Kulhade@users.noreply.github.com>
2017-04-22 20:42:46 -0700

    Update google.strategy.js

--- src/config/strategies/google.strategy.js
+++ src/config/strategies/google.strategy.js
@@ -3,8 +3,8 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 module.exports = function () {
   passport.use(new GoogleStrategy({
-      clientID: '695609297859-5b289kotr7ivfc0rqsi5s2cev4ukh6af.apps.googleusercontent.com',
-      clientSecret: 'QvMvSAbClNoUFlluAF2TP8mb',
+      clientID: '****************',
+      clientSecret: '***************',
       callbackURL: 'http://localhost:5000/auth/google/callback'
     },
     function (req, accessToken, refreshToken, profile, done) {
